312538b5468d9943a33bce0e41f7b8a133630d81
Zotiqq <haxplayer11@gmail.com>
2026-03-12 09:26:56 +0100

    Call method CalcluateMax in main

--- apbs-cw1-git-s33684/apbs-cw1-git-s33684/Program.cs
+++ apbs-cw1-git-s33684/apbs-cw1-git-s33684/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using apbs_cw1_git_s33684;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Feature!");
 Console.WriteLine("Feature2");
 
 Console.WriteLine("Main");
+Statisticshelper.CalculateMax(new []{1, 2, 3});
